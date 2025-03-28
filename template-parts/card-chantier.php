95ffa6d3f1a91c982bcd6c18cc2e4b6dd60d69dd
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-28 11:12:38 +0100

    $value

--- template-parts/card-chantier.php
+++ template-parts/card-chantier.php
@@ -1,12 +1,6 @@
-<?php $taxs = get_the_terms(get_the_id(),'type-chantier');
-    
-if($taxs): 
-    foreach($taxs as $tax):
-        $value = $tax->name;
-    endforeach;
-endif;?>
+<?php $taxs = get_the_terms(get_the_id(),'type-chantier');?>
 
-<a data-filters="<?php echo $value;?>" data-index="<?php echo get_the_id();?>" href="<?php echo get_the_post_thumbnail_url();?>" style="background:url('<?php echo get_the_post_thumbnail_url();?>');" <?php post_class('from-bottom') ;?>>
+<a data-index="<?php echo get_the_id();?>" href="<?php echo get_the_post_thumbnail_url();?>" style="background:url('<?php echo get_the_post_thumbnail_url();?>');" <?php post_class('from-bottom') ;?>>
     <article class="thumbnail">
         <h3><?php the_title();?></h3>
         <p><?php echo get_field('lieu_du_chantier');?></p>
